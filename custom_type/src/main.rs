87ac781b4da2a64d506ce3f07d412d3b047a8b39
XionWin <xionwin@hotmail.com>
2022-07-03 00:58:52 +0800

    Update

--- custom_type/src/main.rs
+++ custom_type/src/main.rs
@@ -1,13 +1,24 @@
 #[allow(dead_code)]
 mod structs;
 
+use structs::*;
+
 fn main() {
     println!("Hello, world!");
 
-    let person = structs::Person {
+    let person = Person {
         name: String::from("Amy"),
         age: 11u8
     };
-
     println!("{:?}", person);
+
+    let unit = Unit;
+    println!("{:?}", unit);
+
+    let pair = Pair(0i32, 0.1f32);
+    println!("{:?}", pair);
+    println!("Pair [0]: {:?},  [1]: {:?}", pair.0, pair.1);
+    let Pair(integer, decimal) = pair;
+    println!("pair contains {:?} and {:?}", integer, decimal);
+
 }
--- custom_type/src/structs.rs
+++ custom_type/src/structs.rs
@@ -3,3 +3,16 @@ pub struct Person {
     pub name: String,
     pub age: u8
 }
+
+// 单元结构体
+#[derive(Debug)]
+pub struct Unit;
+
+// 元组结构体
+#[derive(Debug)]
+pub struct Pair(pub i32, pub f32);
+
+pub struct Point {
+    x: f32,
+    y: f32,
+}
